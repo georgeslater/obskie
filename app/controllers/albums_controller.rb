a756bfdd2e2e1c107ef37fecdf789e9b9b8397e5
George Slater <georgewslater@gmail.com>
2014-11-20 23:53:12 -0200

    Enforce login on new album

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,6 +1,7 @@
 class AlbumsController < ApplicationController
 
  	before_action :set_album, only: [:show, :edit, :update, :destroy]
+ 	before_action :authenticate_user!, only: [:create]
 
 	impressionist :actions=>[:show]
 
