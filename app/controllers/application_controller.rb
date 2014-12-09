eaa3e100887fa2d94b2820ce54bfd973fa5e479a
George Slater <georgewslater@gmail.com>
2014-12-09 01:27:05 -0200

    login menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,25 @@ class ApplicationController < ActionController::Base
   def forem_user
     current_user
   end
+
+  def get_has_drafts
+
+    if current_user.present?
+      @hasDrafts = current_user.albums.select { | album | album.published == false }.size > 0
+    else
+      @hasDrafts = false
+    end
+  end
+
   helper_method :forem_user
+  helper_method :has_drafts
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_filter :get_has_drafts
 
   protected
 
@@ -18,5 +30,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
   end
+
 
 end
