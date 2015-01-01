d59a8cbd70a95908a83b1a37c70dcdd227bbfe49
George Slater <georgewslater@gmail.com>
2015-01-01 17:59:15 -0200

    test mandrill

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,13 +37,12 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-address: 'smtp.gmail.com',
-port: 587,
-domain: ENV['GMAIL_DOMAIN'],
-authentication: 'plain',
-enable_starttls_auto: true,
-user_name: ENV['GMAIL_USERNAME'],
-password: ENV['GMAIL_PASSWORD']
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
 }
 
 
