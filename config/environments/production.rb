569e9bed464c758ab9819681835760f7203759bf
George Slater <georgewslater@gmail.com>
2015-01-01 18:10:43 -0200

    test mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,13 +20,12 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: ENV["GMAIL_DOMAIN"],
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV["GMAIL_USERNAME"],
-  password: ENV["GMAIL_PASSWORD"]
+    :port =>           '587',
+    :address =>        'smtp.mandrillapp.com',
+    :user_name =>      ENV['MANDRILL_USERNAME'],
+    :password =>       ENV['MANDRILL_APIKEY'],
+    :domain =>         'heroku.com',
+    :authentication => :plain
   }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
