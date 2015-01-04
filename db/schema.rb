7e1a4435ac90fd334419e2ef3eaacd10f5b81400
George Slater <georgewslater@gmail.com>
2015-01-04 17:46:15 -0200

    remove extra >

--- db/schema.rb
+++ db/schema.rb
@@ -11,53 +11,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150101174931) do
+ActiveRecord::Schema.define(version: 20150102004345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "albums", force: true do |t|
-    t.string   "title"
-    t.string   "album_art"
+  create_table "albums", force: :cascade do |t|
+    t.string   "title",                  limit: 255
+    t.string   "album_art",              limit: 255
     t.integer  "year"
     t.text     "body"
-    t.string   "spotify_identifier"
+    t.string   "spotify_identifier",     limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "artist_id"
     t.integer  "user_id"
-    t.string   "album_art_file_name"
-    t.string   "album_art_content_type"
+    t.string   "album_art_file_name",    limit: 255
+    t.string   "album_art_content_type", limit: 255
     t.integer  "album_art_file_size"
     t.datetime "album_art_updated_at"
-    t.string   "slug"
+    t.string   "slug",                   limit: 255
     t.integer  "impressions_count"
     t.integer  "obscurity_rating"
-    t.integer  "comments_count",         default: 0
-    t.string   "itunes_identifier"
-    t.string   "itunes_view_url"
-    t.boolean  "published",              default: false, null: false
-    t.string   "amazon_url"
-    t.string   "spotify_link"
-    t.string   "musicbrainz_identifier"
-    t.string   "upc_barcode"
-    t.string   "rdio_url"
-    t.string   "status"
-    t.string   "workflow_state"
+    t.integer  "comments_count",                     default: 0
+    t.string   "itunes_identifier",      limit: 255
+    t.string   "itunes_view_url",        limit: 255
+    t.boolean  "published",                          default: false, null: false
+    t.string   "amazon_url",             limit: 255
+    t.string   "spotify_link",           limit: 255
+    t.string   "musicbrainz_identifier", limit: 255
+    t.string   "upc_barcode",            limit: 255
+    t.string   "rdio_url",               limit: 255
+    t.string   "status",                 limit: 255
+    t.string   "workflow_state",         limit: 255
   end
 
   add_index "albums", ["artist_id"], name: "index_albums_on_artist_id", using: :btree
   add_index "albums", ["user_id"], name: "index_albums_on_user_id", using: :btree
+  add_index "albums", ["workflow_state"], name: "index_albums_on_workflow_state", using: :btree
 
-  create_table "artists", force: true do |t|
-    t.string   "name"
+  create_table "artists", force: :cascade do |t|
+    t.string   "name",                   limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
-    t.string   "musicbrainz_identifier"
+    t.string   "slug",                   limit: 255
+    t.string   "musicbrainz_identifier", limit: 255
   end
 
-  create_table "comments", force: true do |t|
+  create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "album_id"
     t.text     "body"
@@ -70,56 +71,56 @@ ActiveRecord::Schema.define(version: 20150101174931) do
   add_index "comments", ["playlist_id"], name: "index_comments_on_playlist_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
-  create_table "forem_categories", force: true do |t|
-    t.string   "name",                   null: false
+  create_table "forem_categories", force: :cascade do |t|
+    t.string   "name",       limit: 255,             null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "slug"
-    t.integer  "position",   default: 0
+    t.string   "slug",       limit: 255
+    t.integer  "position",               default: 0
   end
 
   add_index "forem_categories", ["slug"], name: "index_forem_categories_on_slug", unique: true, using: :btree
 
-  create_table "forem_forums", force: true do |t|
-    t.string  "name"
+  create_table "forem_forums", force: :cascade do |t|
+    t.string  "name",        limit: 255
     t.text    "description"
     t.integer "category_id"
-    t.integer "views_count", default: 0
-    t.string  "slug"
-    t.integer "position",    default: 0
+    t.integer "views_count",             default: 0
+    t.string  "slug",        limit: 255
+    t.integer "position",                default: 0
   end
 
   add_index "forem_forums", ["slug"], name: "index_forem_forums_on_slug", unique: true, using: :btree
 
-  create_table "forem_groups", force: true do |t|
-    t.string "name"
+  create_table "forem_groups", force: :cascade do |t|
+    t.string "name", limit: 255
   end
 
   add_index "forem_groups", ["name"], name: "index_forem_groups_on_name", using: :btree
 
-  create_table "forem_memberships", force: true do |t|
+  create_table "forem_memberships", force: :cascade do |t|
     t.integer "group_id"
     t.integer "member_id"
   end
 
   add_index "forem_memberships", ["group_id"], name: "index_forem_memberships_on_group_id", using: :btree
 
-  create_table "forem_moderator_groups", force: true do |t|
+  create_table "forem_moderator_groups", force: :cascade do |t|
     t.integer "forum_id"
     t.integer "group_id"
   end
 
   add_index "forem_moderator_groups", ["forum_id"], name: "index_forem_moderator_groups_on_forum_id", using: :btree
 
-  create_table "forem_posts", force: true do |t|
+  create_table "forem_posts", force: :cascade do |t|
     t.integer  "topic_id"
     t.text     "text"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "reply_to_id"
-    t.string   "state",       default: "pending_review"
-    t.boolean  "notified",    default: false
+    t.string   "state",       limit: 255, default: "pending_review"
+    t.boolean  "notified",                default: false
   end
 
   add_index "forem_posts", ["reply_to_id"], name: "index_forem_posts_on_reply_to_id", using: :btree
@@ -127,24 +128,24 @@ ActiveRecord::Schema.define(version: 20150101174931) do
   add_index "forem_posts", ["topic_id"], name: "index_forem_posts_on_topic_id", using: :btree
   add_index "forem_posts", ["user_id"], name: "index_forem_posts_on_user_id", using: :btree
 
-  create_table "forem_subscriptions", force: true do |t|
+  create_table "forem_subscriptions", force: :cascade do |t|
     t.integer "subscriber_id"
     t.integer "topic_id"
   end
 
-  create_table "forem_topics", force: true do |t|
+  create_table "forem_topics", force: :cascade do |t|
     t.integer  "forum_id"
     t.integer  "user_id"
-    t.string   "subject"
+    t.string   "subject",      limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "locked",       default: false,            null: false
-    t.boolean  "pinned",       default: false
-    t.boolean  "hidden",       default: false
+    t.boolean  "locked",                   default: false,            null: false
+    t.boolean  "pinned",                   default: false
+    t.boolean  "hidden",                   default: false
     t.datetime "last_post_at"
-    t.string   "state",        default: "pending_review"
-    t.integer  "views_count",  default: 0
-    t.string   "slug"
+    t.string   "state",        limit: 255, default: "pending_review"
+    t.integer  "views_count",              default: 0
+    t.string   "slug",         limit: 255
   end
 
   add_index "forem_topics", ["forum_id"], name: "index_forem_topics_on_forum_id", using: :btree
@@ -152,13 +153,13 @@ ActiveRecord::Schema.define(version: 20150101174931) do
   add_index "forem_topics", ["state"], name: "index_forem_topics_on_state", using: :btree
   add_index "forem_topics", ["user_id"], name: "index_forem_topics_on_user_id", using: :btree
 
-  create_table "forem_views", force: true do |t|
+  create_table "forem_views", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "viewable_id"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "count",             default: 0
-    t.string   "viewable_type"
+    t.integer  "count",                         default: 0
+    t.string   "viewable_type",     limit: 255
     t.datetime "current_viewed_at"
     t.datetime "past_viewed_at"
   end
@@ -167,11 +168,11 @@ ActiveRecord::Schema.define(version: 20150101174931) do
   add_index "forem_views", ["user_id"], name: "index_forem_views_on_user_id", using: :btree
   add_index "forem_views", ["viewable_id"], name: "index_forem_views_on_viewable_id", using: :btree
 
-  create_table "friendly_id_slugs", force: true do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",           limit: 255, null: false
+    t.integer  "sluggable_id",               null: false
     t.string   "sluggable_type", limit: 50
-    t.string   "scope"
+    t.string   "scope",          limit: 255
     t.datetime "created_at"
   end
 
@@ -180,16 +181,16 @@ ActiveRecord::Schema.define(version: 20150101174931) do
   add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
   add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
-  create_table "impressions", force: true do |t|
-    t.string   "impressionable_type"
+  create_table "impressions", force: :cascade do |t|
+    t.string   "impressionable_type", limit: 255
     t.integer  "impressionable_id"
     t.integer  "user_id"
-    t.string   "controller_name"
-    t.string   "action_name"
-    t.string   "view_name"
-    t.string   "request_hash"
-    t.string   "ip_address"
-    t.string   "session_hash"
+    t.string   "controller_name",     limit: 255
+    t.string   "action_name",         limit: 255
+    t.string   "view_name",           limit: 255
+    t.string   "request_hash",        limit: 255
+    t.string   "ip_address",          limit: 255
+    t.string   "session_hash",        limit: 255
     t.text     "message"
     t.text     "referrer"
     t.datetime "created_at"
@@ -205,36 +206,36 @@ ActiveRecord::Schema.define(version: 20150101174931) do
   add_index "impressions", ["impressionable_type", "message", "impressionable_id"], name: "impressionable_type_message_index", using: :btree
   add_index "impressions", ["user_id"], name: "index_impressions_on_user_id", using: :btree
 
-  create_table "playlists", force: true do |t|
-    t.string   "spotify_uri"
-    t.string   "name"
+  create_table "playlists", force: :cascade do |t|
+    t.string   "spotify_uri",    limit: 255
+    t.string   "name",           limit: 255
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "description"
     t.integer  "followers"
-    t.string   "image_url"
+    t.string   "image_url",      limit: 255
     t.integer  "track_count"
     t.text     "blurb"
-    t.integer  "comments_count", default: 0
-    t.string   "deezer_uri"
-    t.string   "rdio_uri"
-    t.string   "slug"
+    t.integer  "comments_count",             default: 0
+    t.string   "deezer_uri",     limit: 255
+    t.string   "rdio_uri",       limit: 255
+    t.string   "slug",           limit: 255
   end
 
   add_index "playlists", ["slug"], name: "index_playlists_on_slug", using: :btree
 
-  create_table "ratings", force: true do |t|
+  create_table "ratings", force: :cascade do |t|
     t.integer  "track_id"
     t.float    "score"
-    t.string   "ip_address"
+    t.string   "ip_address", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "ratings", ["track_id"], name: "index_ratings_on_track_id", using: :btree
 
-  create_table "reviews", force: true do |t|
+  create_table "reviews", force: :cascade do |t|
     t.text     "body"
     t.integer  "user_id"
     t.datetime "created_at"
@@ -245,48 +246,48 @@ ActiveRecord::Schema.define(version: 20150101174931) do
 
   add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
-  create_table "tracks", force: true do |t|
-    t.string   "name"
+  create_table "tracks", force: :cascade do |t|
+    t.string   "name",                   limit: 255
     t.integer  "album_id"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "order"
     t.float    "author_rating"
     t.integer  "duration_milli"
-    t.string   "spotify_identifier"
-    t.string   "spotify_link"
-    t.string   "musicbrainz_identifier"
-    t.string   "rdio_url"
+    t.string   "spotify_identifier",     limit: 255
+    t.string   "spotify_link",           limit: 255
+    t.string   "musicbrainz_identifier", limit: 255
+    t.string   "rdio_url",               limit: 255
   end
 
   add_index "tracks", ["album_id"], name: "index_tracks_on_album_id", using: :btree
 
-  create_table "users", force: true do |t|
-    t.string   "email",                   default: "",               null: false
-    t.string   "encrypted_password",      default: "",               null: false
-    t.string   "reset_password_token"
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                   limit: 255, default: "",               null: false
+    t.string   "encrypted_password",      limit: 255, default: "",               null: false
+    t.string   "reset_password_token",    limit: 255
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",           default: 0,                null: false
+    t.integer  "sign_in_count",                       default: 0,                null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "current_sign_in_ip",      limit: 255
+    t.string   "last_sign_in_ip",         limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "username"
-    t.string   "first_name"
-    t.string   "last_name"
-    t.boolean  "forem_admin",             default: false
-    t.string   "forem_state",             default: "pending_review"
-    t.boolean  "forem_auto_subscribe",    default: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
+    t.string   "username",                limit: 255
+    t.string   "first_name",              limit: 255
+    t.string   "last_name",               limit: 255
+    t.boolean  "forem_admin",                         default: false
+    t.string   "forem_state",             limit: 255, default: "pending_review"
+    t.boolean  "forem_auto_subscribe",                default: false
+    t.string   "avatar_file_name",        limit: 255
+    t.string   "avatar_content_type",     limit: 255
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.boolean  "admin",                   default: false
-    t.boolean  "is_approved_contributor", default: false
-    t.string   "confirmation_token"
+    t.boolean  "admin",                               default: false
+    t.boolean  "is_approved_contributor",             default: false
+    t.string   "confirmation_token",      limit: 255
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create!(email: "abc@gmail.com", password: "12345678", password_confirmation: "12345678")
+#user = User.create!(email: "obscurealbumsdotcom@gmail.com", username: "Disco Mystic", admin: true, is_approved_contributor: true, password: "12345678")
 
-
-artist = Artist.create(name: 'The Breeders')
-
-album = Album.create(artist_id: artist.id, user_id: user.id, title: 'Last Splash', 
-	year: 1994, album_art: "http://upload.wikimedia.org/wikipedia/en/thumb/f/f3/TheBreedersLastSplash.jpg/220px-TheBreedersLastSplash.jpg",
-	body: "Bacon ipsum dolor sit amet biltong kielbasa ribeye andouille, leberkas short ribs turkey porchetta capicola. Spare ribs meatball 
-	strip steak landjaeger turkey porchetta venison frankfurter bresaola. Andouille pig kevin pastrami tail, strip steak brisket turkey. Sirloin 
-	drumstick pig rump chuck, short ribs ham t-bone strip steak. Cow venison rump, pork belly jowl ribeye kielbasa andouille turducken tri-tip 
-	turkey doner kevin leberkas. Frankfurter capicola turkey meatball ground round sirloin. Tenderloin pancetta brisket tri-tip.",
-	spotify_identifier: "4lxW0axOKlImAQ0akMRz61")
