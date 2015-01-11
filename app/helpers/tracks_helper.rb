427715900430effd4b2cd6338d72f909f0fb2be4
George Slater <georgewslater@gmail.com>
2015-01-11 01:37:07 -0200

    microdata schema.org

--- app/helpers/tracks_helper.rb
+++ app/helpers/tracks_helper.rb
@@ -23,4 +23,26 @@ module TracksHelper
 
 	  	return time
   	end
+
+  	def get_schema_duration(timeMilli)
+
+  		time = ""
+
+  		if timeMilli != nil
+  		
+  			time += "PT" + ((timeMilli / 1000) / 60).to_s + "M"
+
+  			secondsPart = ((timeMilli / 1000) % 60).to_s
+	  		
+	  		if secondsPart.length == 0
+	  			time += '00'
+	  		elsif secondsPart.length == 1
+	  			time += '0'
+	  		end
+
+	  		time += secondsPart + "S"
+	  	end
+
+	  	return time
+  	end
 end
