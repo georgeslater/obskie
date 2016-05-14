d5d9bdcda0d675d3e327cdd531d63adf3c78f2f0
George Slater <georgewslater@gmail.com>
2016-05-13 23:55:18 -0300

    redis memory fixes

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,11 +1,11 @@
-require 'redis'
+#require 'redis'
 
-REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
+#REDIS = Redis.connect(url: ENV['REDISTOGO_URL'])
 
 Geocoder.configure(
  timeout: 10,
  ip_lookup: :google,
- cache: REDIS,
+ #cache: REDIS,
  always_raise: [
     Geocoder::OverQueryLimitError,
     Geocoder::RequestDenied,
